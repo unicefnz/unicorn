5bdf0a4f10c499d044c24ad5dd3cf564097f0ea3
Asher Foster <asher@unicef.org.nz>
2020-06-19 13:12:25 +1200

    Remove addon-info, revert removing margins

--- core/.storybook/main.js
+++ core/.storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   addons: [
     '@storybook/preset-typescript',
     '@storybook/addon-actions',
-    '@storybook/addon-info',
     '@storybook/addon-knobs'
   ],
   stories: ['../src/**/*.stories.ts(|x)']
