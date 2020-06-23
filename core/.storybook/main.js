7bfd751c3804c254ac4e7ea5538c39404da57743
Asher Foster <asher@unicef.org.nz>
2020-06-23 12:10:25 +1200

    [core] Disable babel-plugin-transform-simplify-comparison-operators
    See https://github.com/storybookjs/storybook/issues/11041#issuecomment-647830443

--- core/.storybook/main.js
+++ core/.storybook/main.js
@@ -4,5 +4,13 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-knobs'
   ],
-  stories: ['../src/**/*.stories.ts(|x)']
+  stories: ['../src/**/*.stories.ts(|x)'],
+  webpackFinal(config) {
+    const jsRule = config.module.rules[0];
+    const babelPresets = jsRule.use.find(l => l.loader === 'babel-loader').options.presets;
+    const minifyPreset = babelPresets[1];
+    minifyPreset[1].simplifyComparisons = false;
+
+    return config;
+  }
 };
