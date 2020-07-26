01fc500d2c7f05b92dc286a96f11d0f9c1b244fc
Asher Foster <asher@unicef.org.nz>
2020-07-27 10:38:47 +1200

    Apply polyfills in storybook

--- core/.storybook/preview.js
+++ core/.storybook/preview.js
@@ -1,4 +1,5 @@
 import '!style-loader!css-loader!sass-loader!../src/css/prebuilt/unicef.scss';
-import { defineCustomElements } from '../dist/esm/loader';
+import { applyPolyfills, defineCustomElements } from '../dist/esm/loader';
 
-defineCustomElements();
+if (!window.customElements) applyPolyfills().then(() => defineCustomElements());
+else defineCustomElements();
