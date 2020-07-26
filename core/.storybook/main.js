30d9f8dc87b12f14d57bb7e0665f51302576027d
Asher Foster <asher@unicef.org.nz>
2020-07-27 11:44:35 +1200

    Disable babel-minify, update deps

--- core/.storybook/main.js
+++ core/.storybook/main.js
@@ -8,8 +8,9 @@ module.exports = {
   webpackFinal(config) {
     const jsRule = config.module.rules[0];
     const babelPresets = jsRule.use.find(l => l.loader === 'babel-loader').options.presets;
-    const minifyPreset = babelPresets[1];
-    minifyPreset[1].simplifyComparisons = false;
+
+    // Babel-minify causes lots of issues with Stencil, so disable it and rely on Terser
+    babelPresets.splice(1, 1);
 
     return config;
   }
