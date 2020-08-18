ee8c1843b0de068b9173c77569d9df69e591899a
Asher Foster <asher@unicef.org.nz>
2020-08-18 14:45:20 +1200

    Fix React tree issues in scoped mode
    Fixes FRNTND-61

--- packages/react/src/components/utils/createOverlayComponent.tsx
+++ packages/react/src/components/utils/createOverlayComponent.tsx
@@ -12,6 +12,12 @@ export function createOverlayComponent<
 
   function ReactOverlayComponent({ isOpen, children, ...restProps }: Props) {
     const [overlayEl, setOverlayEl] = useState<OverlayEl | null>(null);
+    // We could render the React tree directly under the overlayEl
+    // However, in scoped mode overlayEl's children get relocated down the tree
+    // which breaks the bond between render root and children.
+    // To fix this, we render to a intermediate div, and place that inside the overlayEl
+    // No matter where this div moves, React doesn't care because the child tree remains intact
+    const renderRoot = useRef<HTMLDivElement>(document.createElement('div'));
     const prevPropsRef = useRef<Omit<Props, 'isOpen'|'children'>>(); // Keep track of the last props, used for dom binding.
 
     useEffect(() => {
@@ -27,6 +33,7 @@ export function createOverlayComponent<
             setOverlayEl(el);
 
             attachProps(el, restProps);
+            el.append(renderRoot.current);
 
             el.present();
           });
@@ -48,7 +55,7 @@ export function createOverlayComponent<
     }, [restProps]);
 
     if (overlayEl) {
-      return ReactDOM.createPortal(children, overlayEl);
+      return ReactDOM.createPortal(children, renderRoot.current);
     }
     return null;
   }
