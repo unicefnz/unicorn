fc4fe303ff3971b3a6e31d12e4d0347d79028c36
Asher Foster <asher@unicef.org.nz>
2020-09-28 15:35:07 +1300

    Add a monster main pipeline

---
+++ scripts/publish.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Based on Next.js's CI pipeline. Thanks Vercel!
+# https://github.com/vercel/next.js/blob/canary/publish-release.sh
+
+git describe --exact-match
+
+if [[ ! $? -eq 0 ]];then
+  echo "Nothing to publish, exiting.."
+  exit 0;
+fi
+
+if [[ -z "$NPM_TOKEN" ]];then
+  echo "No NPM_TOKEN, exiting.."
+  exit 0;
+fi
+
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+
+exit 0; # TODO Remove, exiting early while testing the new pipeline
+
+if [[ $(git describe --exact-match 2> /dev/null || :) =~ -beta ]];then
+  echo "Publishing beta"
+  lerna publish from-git --npm-tag beta --yes
+
+  # Make sure to exit script with code 1 if publish failed
+  if [[ ! $? -eq 0 ]];then
+    exit 1;
+  fi
+else
+  echo "Did not publish beta"
+fi
+
+if [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -beta ]];then
+  echo "Publishing stable"
+  lerna publish from-git --yes
+
+  # Make sure to exit script with code 1 if publish failed
+  if [[ ! $? -eq 0 ]];then
+    exit 1;
+  fi
+else
+  echo "Did not publish stable"
+fi
