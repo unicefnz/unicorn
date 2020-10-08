71623cbc7b18ef67b1f660b210ccde0dde9973b6
Asher Foster <asher@unicef.org.nz>
2020-10-08 14:33:34 +1300

    Chore(CI): Switch to angular preset

--- scripts/release.sh
+++ scripts/release.sh
@@ -25,4 +25,4 @@ echo Creating a new ${SEMVER_BUMP} version
 # - Create a github release with said changelog
 # - Commit & tag changes
 # - Push commit & tag
-lerna version "${SEMVER_BUMP}" --preid beta --create-release github --conventional-commits --changelog-preset eslint --message "Chore: Release %s 🎉" ${EXTRA_ARGS}
+lerna version "${SEMVER_BUMP}" --preid beta --create-release github --conventional-commits --changelog-preset angular --message "Chore: Release %s 🎉" ${EXTRA_ARGS}
