1d1316341a05f31a2c9a0c9031ec072f5b4349e9
Asher Foster <asher@unicef.org.nz>
2020-10-21 13:07:31 +1300

    fix(react): Ensure custom elements aren't defined twice

--- packages/react/src/utils/defineElement.ts
+++ packages/react/src/utils/defineElement.ts
@@ -1,4 +1,8 @@
 export function defineElement(cls: CustomElementConstructor): void {
+  // In dev mode, check this is only used with stencil elements
   if (process.env.NODE_ENV === 'development' && !(cls as any).is) throw new Error('Class does not have metadata set, this method can only be used for Stencil elements');
-  window.customElements.define((cls as any).is, cls);
+  // Define the element if it doesn't already exist
+  if (!window.customElements.get((cls as any).is)) {
+    window.customElements.define((cls as any).is, cls);
+  }
 }
