3f8718a54866a594eaff2e9f0fcaa2b5a88100d3
Asher Foster <asher@unicef.org.nz>
2021-03-31 14:18:12 +1300

    chore(deps): Update core/react deps
    Also fix dialog spec test

--- core/src/components/uni-dialog/test/uni-dialog.spec.tsx
+++ core/src/components/uni-dialog/test/uni-dialog.spec.tsx
@@ -9,10 +9,14 @@ describe('uni-dialog', () => {
     });
     expect(page.root).toEqualHtml(`
       <uni-dialog aria-modal="true" id="uni-overlay-1" tabindex="-1" uni-overlay="" role="dialog" style="z-index: 2001;">
-        <uni-backdrop tappable=""></uni-backdrop>
-        <div class="flex-wrapper">
-          <uni-dialog-content></uni-dialog-content>
-        </div>
+        <mock:shadow-root>
+          <uni-backdrop tappable=""></uni-backdrop>
+          <div class="flex-wrapper">
+            <uni-dialog-content>
+              <slot />
+            </uni-dialog-content>
+          </div>
+        </mock:shadow-root>
       </uni-dialog>
     `);
   });
